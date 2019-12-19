fe4a4f61f851ec270b548b1c8a588608322d281b
codersguild <shekharhiran@hotmail.com>
2019-12-19 23:16:50 +0530

    Parser passes

--- code/src/main/scala/parser/parser.scala
+++ code/src/main/scala/parser/parser.scala
@@ -2,9 +2,22 @@ package parser
 
 import scala.collection.mutable._
 import scala.collection.immutable._
+import org.antlr.v4.runtime._
 import scala.io._
-    
+import simpl._
+ 
 object parser {
     
-    def parse (expr : String) : 
+    def parse (expr : String) = {
+
+        var parseStream = new ANTLRInputStream (expr);
+        var lexer = new simplLexer (parseStream);
+        var tokens = new CommonTokenStream (lexer);
+        var parseEngine = new simplParser (tokens); 
+    
+    }
+
+    class SimplExprParsing extends simplBaseListener {
+
+    }
 }
